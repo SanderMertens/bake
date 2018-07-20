0fd3010054d8010251be03b3fe728597efb52b0e
Sander Mertens <sander.mertens8@gmail.com>
2018-07-19 23:28:57 -0700

    Add get_attr_bool function to project type

--- include/bake/project.h
+++ include/bake/project.h
@@ -111,6 +111,7 @@ typedef struct bake_project {
     /* Interface for bake plugin */
     bake_project_attr* (*get_attr)(const char *name);
     char* (*get_attr_string)(const char *name);
+    bool (*get_attr_bool)(const char *name);
     void (*add_build_dependency)(const char *file);
     void (*clean)(const char *file);
 } bake_project;
