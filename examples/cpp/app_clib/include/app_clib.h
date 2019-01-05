48d5d6fd7d88b0208605a5c22b123e50b61da43b
Sander Mertens <sander.mertens8@gmail.com>
2019-01-05 12:46:13 -0800

    Add app_clib example for C++

---
+++ examples/cpp/app_clib/include/app_clib.h
@@ -0,0 +1,9 @@
+#ifndef APP_CLIB_H
+#define APP_CLIB_H
+
+/* This generated file contains includes for project dependencies */
+#include "bake_config.h"
+
+
+#endif
+
---
+++ examples/cpp/app_clib/src/main.cpp
@@ -0,0 +1,8 @@
+#include <include/app_clib.h>
+#include <iostream>
+#include <math.h>
+
+int main(int argc, char *argv[]) {
+    std::cout << "cos(1.0) = " << cos(1.0) << std::endl;
+    return 0;
+}
