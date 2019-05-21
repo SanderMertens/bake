5172e3892179190b6d72ff90f3758d46299b5dc8
Sander Mertens <sander.mertens8@gmail.com>
2019-05-20 18:39:11 -0700

    setup didn't use correct bake executable everywhere

--- src/setup.c
+++ src/setup.c
@@ -220,7 +220,7 @@ int16_t bake_build_make_project(
     const char *artefact)
 {
     char *install_cmd = ut_asprintf(
-      "bake install %s --id %s --package --includes include",
+      "."UT_OS_PS"bake install %s --id %s --package --includes include",
       path, id);
     ut_try( cmd(install_cmd), "failed to install '%s' include files", id);
     free(install_cmd);
@@ -275,7 +275,7 @@ int16_t bake_build_make_project(
     free(bin_path);
 
     install_cmd = ut_asprintf(
-        "bake install %s --id %s --artefact %s --package",
+        "."UT_OS_PS"bake install %s --id %s --artefact %s --package",
         path, id, artefact);
     ut_try(cmd(install_cmd), "failed to install bake %s library", id);
 
@@ -334,7 +334,7 @@ int16_t bake_setup(
 
     bake_message(UT_OK, "done", "copy bake executable");
 
-    ut_try( cmd("bake install --id bake --includes include"),
+    ut_try( cmd("."UT_OS_PS"bake install --id bake --includes include"),
         "failed to install bake include files");
     bake_message(UT_OK, "done", "install bake include files");
 
