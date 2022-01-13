e52373f92f45ae66980601c0d1602af263e8ddf8
Sander Mertens <sander.mertens8@gmail.com>
2022-01-13 00:08:13 -0800

    Use project name with underscores for webasm module

--- drivers/lang/c/src/gcc/driver.c
+++ drivers/lang/c/src/gcc/driver.c
@@ -395,7 +395,7 @@ void add_misc_link(
         ut_strbuf_append(cmd, " -s ALLOW_MEMORY_GROWTH=1");
         ut_strbuf_append(cmd, " -s EXPORTED_RUNTIME_METHODS=cwrap");
         ut_strbuf_append(cmd, " -s MODULARIZE=true");
-        ut_strbuf_append(cmd, " -s EXPORT_NAME=\"%s\"", project->id);
+        ut_strbuf_append(cmd, " -s EXPORT_NAME=\"%s\"", project->id_underscore);
 
         if (ut_file_test("etc/assets") == 1) {
             ut_strbuf_append(cmd, " --embed-file etc/assets");
