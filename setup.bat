d8a38f4b551f7f4a223864db3c02682a12ddc128
Ed Preston <epreston@prestonsoft.com>
2024-07-31 19:02:41 +1000

    fix: setup.bat
    escape of parenthesis in quoted string not required, it causes directory match to fail when it exists. it is required when setting the environment variable in the following line.

--- setup.bat
+++ setup.bat
@@ -23,7 +23,7 @@ rem If not found, try in x86 program files
 if "%VSDIR%"=="" (
     for /l %%v in (2050, -1, 2015) do (
         if "%VSDIR%"=="" (
-          if exist "C:\Program Files (x86^)\Microsoft Visual Studio\%%v\Community\" (
+          if exist "C:\Program Files (x86)\Microsoft Visual Studio\%%v\Community\" (
               set VSDIR=C:\Program Files (x86^)\Microsoft Visual Studio\%%v\Community
           )
         )
